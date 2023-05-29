a225023609d8f9b4246d76621d83b76193698bfb
Ilia A <abrilles01@gmail.com>
2023-05-29 19:58:55 +0400

    Bot check eth price and send alert.Update sql functions

--- sql.py
+++ sql.py
@@ -15,9 +15,29 @@ async def create_db():
 async def add_gas_price(alert_gas_price, chat_id):
     with sqlite3.connect('gas_db.sqlite') as conn:
         cur = conn.cursor()
-        cur.execute(f'INSERT INTO users_gas_alert (alert_gas_price, chat_id)'
-                    f'VALUES ({alert_gas_price}, {chat_id})')
-        conn.commit()
+
+        cur.execute("SELECT chat_id FROM users_gas_alert WHERE chat_id = ?", (chat_id,))
+        row = cur.fetchone()
+
+        if row:
+            cur.execute("UPDATE users_gas_alert SET alert_gas_price = ?"
+                        "WHERE chat_id = ?", (alert_gas_price, chat_id))
+            conn.commit()
+        else:
+            cur.execute("INSERT INTO users_gas_alert (alert_gas_price,"
+                        "chat_id) VALUES (?, ?)", (alert_gas_price, chat_id))
+            conn.commit()
+
+
+
+
+
+async def check_gas_price(eth_price):
+    with sqlite3.connect('gas_db.sqlite') as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT chat_id FROM users_gas_alert WHERE alert_gas_price <= ?", (eth_price,))
+        rows = cur.fetchall()
+        return rows
 
 async def check_table():
     conn = sqlite3.connect('gas_db.sqlite')
@@ -41,8 +61,3 @@ loop.run_until_complete(check_table())
 
 # Close the event loop
 loop.close()
-
-
-
-async def check_gas_price():
-    pass
--- test2.py
+++ test2.py
@@ -1,5 +1,6 @@
 import aiohttp
 import os
+import asyncio
 import telegram
 from telegram.ext import (
     ApplicationBuilder,
@@ -11,7 +12,8 @@ from telegram.ext import (
 )
 
 from dotenv import load_dotenv
-from sql import create_db, add_gas_price
+from sql import create_db, add_gas_price, check_gas_price
+from logger import logger
 
 load_dotenv()
 
@@ -32,20 +34,24 @@ async def start(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
                                    text="I'm a bot!",
                                    reply_markup=keyboard)
 
-
-async def get_eth_price(update: telegram.Update,
-                        context: ContextTypes.DEFAULT_TYPE):
+async def eth_price(context: ContextTypes.DEFAULT_TYPE):
+    global ETH_PRICE
     async with aiohttp.ClientSession() as session:
         async with session.get(ETHER_PRICE_URL) as resp:
             data = await resp.json()
-            eth_price = data['result']['ethusd']
-            await context.bot.send_message(chat_id=update.effective_chat.id,
-                                           text=eth_price)
+            ETH_PRICE = data['result']['ethusd']
+
+
+
+async def get_eth_price(update: telegram.Update,
+                        context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text=ETH_PRICE)
 
 
 async def eth_alert(update: telegram.Update,
                     context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Please enter a value in 'int' format:")
+    await update.message.reply_text("Please enter a value in 'int' format")
     return CHOOSING
 
 
@@ -59,11 +65,23 @@ async def received_value(update: telegram.Update,
             await add_gas_price(alert_gas_price, chat_id)
             return ConversationHandler.END
         except ValueError:
-            await update.message.reply_text("Invalid value.")
+            await update.message.reply_text("Error.Enter value in 'int'")
+
+
+async def send_alert(context: ContextTypes.DEFAULT_TYPE):
+    chat_ids = await check_gas_price(ETH_PRICE)
+    async for chat_id in chat_ids:
+        await context.bot.send_message(chat_id=chat_id[0], text=f'Eth price is {ETH_PRICE}')
 
 
 def main():
     application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+
+    job_queue = application.job_queue
+    job_queue.run_repeating(callback=send_alert, interval=300, first=3)
+
+    job_queue = application.job_queue
+    job_queue.run_repeating(callback=eth_price, interval=33, first=1)
 
     eth_alert_conversation_handler = ConversationHandler(
         entry_points=[CommandHandler("eth_alert", eth_alert)],
@@ -76,6 +94,7 @@ def main():
     application.add_handler(CommandHandler('start', start))
     application.add_handler(CommandHandler("get_eth_price", get_eth_price))
     application.add_handler(eth_alert_conversation_handler)
+
     application.run_polling()
 
 
